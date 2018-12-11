e94a52f9bb49889a540c9015775dee9f8f1fb456
Andy Newton <andy@arin.net>
2018-12-11 15:32:32 -0500

    initial

---
+++ src/main/kotlin/com/rcode3/cp/Basic3.kt
@@ -0,0 +1,17 @@
+package com.rcode3.cp
+
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking {
+    //sampleStart
+    val job = GlobalScope.launch { // launch new coroutine and keep a reference to its Job
+        delay(1000L)
+        println("World!")
+    }
+    println("Hello,")
+    job.join() // wait until child coroutine completes
+//sampleEnd
+}
